7768fc0b90614ab4b554ddf5a7e995eb41a6b11b
Yamakasi <believelody@gmail.com>
2016-10-25 17:50:50 +0200

    Initial checkin for sales tool.

---
+++ GitTuto/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesData data = new SalesData();
+		
+		displayGreetings();
+		data.display();
+
+	}
+	
+	public static void displayGreetings(){
+		System.out.println("Hello happy sales people");
+		System.out.println("This app shows sales data");
+	}
+
+}
